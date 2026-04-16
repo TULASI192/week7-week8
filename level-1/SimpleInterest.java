8b9b65c34290422517b9f4b92b89f42a7ea57b30
TULASI192 <tulasigovada77@gmail.com>
2026-04-16 08:51:17 +0530

    added level1 problemm1

---
+++ level-1/SimpleInterest.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+//leve1 
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("something");
+
+        System.out.print("Enter Principal: ");
+        double principal = sc.nextDouble();
+        System.out.print("Enter Rate: ");
+        double rate = sc.nextDouble();
+        System.out.print("Enter Time (years): ");
+        double time = sc.nextDouble();
+
+        double interest = calculateInterest(principal, rate, time);
+
+        System.out.println("The Simple Interest is " + interest + 
+                           " for Principal " + principal + 
+                           ", Rate of Interest " + rate + 
+                           " and Time " + time);
+        sc.close();
+    }
+
+    // Method to calculate simple interest
+    public static double calculateInterest(double p, double r, double t) {
+        return (p * r * t) / 100;
+    }
+}
