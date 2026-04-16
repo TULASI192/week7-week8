37119bc30f7cba38430c7133b21731f9e8c45700
TULASI192 <tulasigovada77@gmail.com>
2026-04-16 08:53:38 +0530

    added level1

--- level-1/SimpleInterest.java
+++ level-1/SimpleInterest.java
@@ -4,7 +4,7 @@ public class SimpleInterest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("something");
-
+//level1
         System.out.print("Enter Principal: ");
         double principal = sc.nextDouble();
         System.out.print("Enter Rate: ");
